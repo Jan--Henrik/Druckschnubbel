f269db5aae4f1c7bd43be0eb231562d618dce0c9
JanHenrik <Jan--Henrik>
2016-11-26 22:23:11 +0000

    update

--- creator/yandere.sh
+++ creator/yandere.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cd /home/janhenrik/Druckschnubbel/app/uploads/
+cd ../app/uploads/
 
 TAG="$1"
+TAG="${TAG/ /_}"
+TAG="${TAG/\"/}"
 [ -z "$TAG" ] && exit 1
 
-POST=$(wget -q -O- "https://yande.re/post?tags=${TAG}+order%3Arandom+rating%3Asafe+score%3A20.." | grep -E -o 'href="/post/show/[0-9]+' | head -n1 | cut -d '/' -f4)
-URL=$(wget -q -O- "https://yande.re/post/show/${POST}" | grep 'Post.register_resp(' | grep '"id":'$POST | grep -o -E '"file_url":"http[^"]+"' | cut -d'"' -f4)
+URL=$(wget -q -O- "https://yande.re/post.json?limit=1&tags=${TAG}+order%3Arandom+rating%3Asafe+score%3A20.." | grep -o -E '"file_url":"http[^"]+"' | cut -d'"' -f4)
 wget -O $(date +%N) "$URL"
 exit $?
