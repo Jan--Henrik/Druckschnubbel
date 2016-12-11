2efb7a3102692f620ebe6f7224b881a846aec9d1
jan--henrik <janhenrik@janhenrik.org>
2016-12-11 16:59:06 +0100

    fixed cyberband

--- creator/cyber.py
+++ creator/cyber.py
@@ -8,13 +8,13 @@ def printCyber(length):
     imgwhite = img = Image.new("RGB", (1280, 960), "white") # create an image object
     for x in range(0,length):
         imgwhite.save("app/uploads/imgw.png")
-        time.sleep(10)
-        for y in range(1,5):
+        #time.sleep(10)
+        for y in range(1,6):
             img = Image.open("creator/data/cyber%i.png" % (y))
             img.save("app/uploads/cyber%i.png" % (y))
-            time.sleep(10)
-	imgwhite.save("app/uploads/imgw.png")
-        time.sleep(10)
+            #time.sleep(10)
+        imgwhite.save("app/uploads/imgw.png")
+        #time.sleep(10)
 
 
 if __name__ == '__main__': # if used as single programm
